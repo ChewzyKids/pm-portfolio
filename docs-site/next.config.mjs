58df978199dd9e442cb6802dcd793e10adc7bc19
josephmbryantjr123 <josephmbryantjr@gmail.com>
2025-10-02 14:59:21 -0700

    Initial commit: clean portfolio (no node_modules/.next)

---
+++ docs-site/next.config.mjs
@@ -0,0 +1,6 @@
+import nextra from 'nextra'
+const withNextra = nextra({
+theme: 'nextra-theme-docs',
+themeConfig: './theme.config.mjs'
+})
+export default withNextra()
---
+++ docs-site/theme.config.mjs
@@ -0,0 +1,5 @@
+export default {
+logo: 'Joseph M. Bryant – Portfolio',
+project: { link: 'https://github.com/ChewzyKids/pm-portfolio' },
+footer: { text: '© Joseph M. Bryant' }
+}
