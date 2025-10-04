dfc72cc63dc0058846faaf297987b02643bf7273
Joseph <josephmbryantjr@gmail.com>
2025-10-04 15:42:48 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
-import SEO from '../lib/seo.config'          // no @ alias
+import SEO from '../lib/seo.config'      // note the relative path (no "@")
 import '../styles/globals.css'
-import '../styles/nextra-tweaks.css'          // keep if you still have this file
+import '../styles/nextra-tweaks.css'      // keep if this file exists
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
