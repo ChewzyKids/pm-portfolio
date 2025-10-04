7caefccfdca13189cf9050e9fe0169a672a4b424
Joseph <josephmbryantjr@gmail.com>
2025-10-03 18:20:56 -0700

    Create theme.config.tsx

---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default {
+  logo: <span>Joseph M. Bryant</span>,
+  project: {
+    link: 'https://github.com/ChewyzKids/pm-portfolio',
+  },
+  docsRepositoryBase: 'https://github.com/ChewyzKids/pm-portfolio/blob/main',
+  footer: {
+    text: '© 2025 Joseph M. Bryant – PM Portfolio',
+  },
+}
