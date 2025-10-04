77a6cdcda1c2fb14af5f20200c11139b0320c7eb
Joseph <josephmbryantjr@gmail.com>
2025-10-03 20:07:53 -0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+// docs-site/theme.config.tsx
+import type { DocsThemeConfig } from 'nextra-theme-docs'
 
-export default {
-  logo: <span>Joseph M. Bryant</span>,
-  project: {
-    link: 'https://github.com/ChewyzKids/pm-portfolio',
-  },
-  docsRepositoryBase: 'https://github.com/ChewyzKids/pm-portfolio/blob/main',
-  footer: {
-    text: '© 2025 Joseph M. Bryant – PM Portfolio',
-  },
+const config: DocsThemeConfig = {
+  logo: <span>Joseph M. Bryant – Portfolio</span>,
+  project: { link: 'https://github.com/ChewzyKids/pm-portfolio' },
+  docsRepositoryBase: 'https://github.com/ChewzyKids/pm-portfolio/tree/main/docs-site',
+  head: (
+    <>
+      <meta property="og:image" content="/og.png" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  ),
+  footer: { text: '© Joseph M. Bryant' }
 }
+
+export default config
