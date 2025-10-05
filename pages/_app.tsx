b32453841046a4ad748f9efbd8f1f6c98ccb7dcd
Joseph <josephmbryantjr@gmail.com>
2025-10-04 23:33:27 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import type { AppProps } from 'next/app'
-import { DefaultSeo } from 'next-seo'
-import SEO from '../lib/seo.config'
-import '../styles/globals.css'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
