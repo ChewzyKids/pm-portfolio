1cbe829f17df6a04663e97aa3480287381a626df
Joseph <josephmbryantjr@gmail.com>
2025-10-06 07:02:51 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-plugins: {
-tailwindcss: {},
-autoprefixer: {}
-}
-}
+/** PostCSS (ESM) */
+export default {
+  plugins: {
+    // tailwindcss: {},
+    'postcss-import': {},
+    autoprefixer: {}
+  }
+};
